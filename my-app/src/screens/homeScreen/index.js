758810e57d95eafb27dfe6600f726caf3db72466
peek <sar.srijan@gmail.com>
2024-08-01 11:46:05 +0530

    homescreen left component completed

--- my-app/src/screens/homeScreen/index.js
+++ my-app/src/screens/homeScreen/index.js
@@ -1,13 +1,19 @@
 import "./index.scss"
+import { RightComponent } from "./rightComponent";
 export const HomeScreen = () => {
     return (
         <div className="home-container">
             <div className="left-container">
-                <img src="Kalvium-Logo-SVG.png" />
+                <div className="items-container">
+                    <img src="Kalvium-Logo-SVG.png" />
+                    {/* <h2>code.compile.debug</h2> */}
+                    <button>
+                    <span class="material-icons">add</span>
+                    <span>Create Folder</span>
+                    </button>
+                </div>
             </div>
-            <div className="right-container">
-                <h1>right container</h1> 
-            </div>
+            <RightComponent />
         </div>
     );
 }
---
+++ my-app/src/screens/homeScreen/rightComponent/index.js
@@ -0,0 +1,11 @@
+import "./index.scss"
+export const RightComponent = () => {
+    return <div className="right-container">
+        <div className="header">
+            <div className="title">My playground</div>
+            <button className="add-folder">
+                <span className="material-icons">add</span>
+            </button>
+        </div>
+    </div>
+}
