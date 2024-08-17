aa9bf5fc4d5cf0549b6f296083be8364fc5d3485
peek <sar.srijan@gmail.com>
2024-08-18 03:36:05 +0530

    feat: added file cards to homescreen

--- my-app/src/screens/homeScreen/rightComponent/index.js
+++ my-app/src/screens/homeScreen/rightComponent/index.js
@@ -7,5 +7,34 @@ export const RightComponent = () => {
                 <span className="material-icons">add</span>
             </button>
         </div>
+        <div className="folder-container">
+            <div className="folder-header">
+            {/* separating the header into two different divs to use justify content space between*/}
+                <div className="folder-header-item">
+                    <span className="material-icons">folder</span>
+                    <span>{"DSA"}</span>
+                </div>
+                <div className="folder-header-item">
+                    <span className="material-icons">delete</span>
+                    <span className="material-icons">edit</span>
+                    <button>
+                        <span className="material-icons">add</span>
+                    </button>
+                </div>
+            </div>
+            <div className="cards-container">
+                <div className="card">
+                    <img src = "brave_xCZFNihyOb-removebg-preview.png"></img>
+                    <div className="title-container">
+                        <span>{"heap implementation"}</span>
+                        <span>Language: {"Java"}</span>
+                    </div>
+                    <div className="card-item">
+                        <span className="material-icons">edit</span>
+                        <span className="material-icons">delete</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 }
