6874fcb9979d5d39008240e578fbafa388a64c74
peek <sar.srijan@gmail.com>
2024-08-18 22:03:58 +0530

    feat: added cards to homescreen

--- my-app/src/screens/homeScreen/rightComponent/index.js
+++ my-app/src/screens/homeScreen/rightComponent/index.js
@@ -15,14 +15,171 @@ export const RightComponent = () => {
                     <span>{"DSA"}</span>
                 </div>
                 <div className="folder-header-item">
-                    <span className="material-icons">delete</span>
                     <span className="material-icons">edit</span>
+                    <span className="material-icons">delete</span>
                     <button>
                         <span className="material-icons">add</span>
                     </button>
                 </div>
             </div>
             <div className="cards-container">
+                <div className="card">
+                    <img src = "brave_xCZFNihyOb-removebg-preview.png"></img>
+                    <div className="title-container">
+                        <span>{"heap implementation"}</span>
+                        <span>Language: {"Java"}</span>
+                    </div>
+                    <div className="card-item">
+                        <span className="material-icons">edit</span>
+                        <span className="material-icons">delete</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src = "brave_xCZFNihyOb-removebg-preview.png"></img>
+                    <div className="title-container">
+                        <span>{"heap implementation"}</span>
+                        <span>Language: {"Java"}</span>
+                    </div>
+                    <div className="card-item">
+                        <span className="material-icons">edit</span>
+                        <span className="material-icons">delete</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src = "brave_xCZFNihyOb-removebg-preview.png"></img>
+                    <div className="title-container">
+                        <span>{"heap implementation"}</span>
+                        <span>Language: {"Java"}</span>
+                    </div>
+                    <div className="card-item">
+                        <span className="material-icons">edit</span>
+                        <span className="material-icons">delete</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src = "brave_xCZFNihyOb-removebg-preview.png"></img>
+                    <div className="title-container">
+                        <span>{"heap implementation"}</span>
+                        <span>Language: {"Java"}</span>
+                    </div>
+                    <div className="card-item">
+                        <span className="material-icons">edit</span>
+                        <span className="material-icons">delete</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="folder-container">
+            <div className="folder-header">
+            {/* separating the header into two different divs to use justify content space between*/}
+                <div className="folder-header-item">
+                    <span className="material-icons">folder</span>
+                    <span>{"class practice"}</span>
+                </div>
+                <div className="folder-header-item">
+                    <span className="material-icons">edit</span>
+                    <span className="material-icons">delete</span>
+                    <button>
+                        <span className="material-icons">add</span>
+                    </button>
+                </div>
+            </div>
+            <div className="cards-container">
+                <div className="card">
+                    <img src = "brave_xCZFNihyOb-removebg-preview.png"></img>
+                    <div className="title-container">
+                        <span>{"heap implementation"}</span>
+                        <span>Language: {"Java"}</span>
+                    </div>
+                    <div className="card-item">
+                        <span className="material-icons">edit</span>
+                        <span className="material-icons">delete</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src = "brave_xCZFNihyOb-removebg-preview.png"></img>
+                    <div className="title-container">
+                        <span>{"heap implementation"}</span>
+                        <span>Language: {"Java"}</span>
+                    </div>
+                    <div className="card-item">
+                        <span className="material-icons">edit</span>
+                        <span className="material-icons">delete</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src = "brave_xCZFNihyOb-removebg-preview.png"></img>
+                    <div className="title-container">
+                        <span>{"heap implementation"}</span>
+                        <span>Language: {"Java"}</span>
+                    </div>
+                    <div className="card-item">
+                        <span className="material-icons">edit</span>
+                        <span className="material-icons">delete</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src = "brave_xCZFNihyOb-removebg-preview.png"></img>
+                    <div className="title-container">
+                        <span>{"heap implementation"}</span>
+                        <span>Language: {"Java"}</span>
+                    </div>
+                    <div className="card-item">
+                        <span className="material-icons">edit</span>
+                        <span className="material-icons">delete</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="folder-container">
+            <div className="folder-header">
+            {/* separating the header into two different divs to use justify content space between*/}
+                <div className="folder-header-item">
+                    <span className="material-icons">folder</span>
+                    <span>{"class practice"}</span>
+                </div>
+                <div className="folder-header-item">
+                    <span className="material-icons">edit</span>
+                    <span className="material-icons">delete</span>
+                    <button>
+                        <span className="material-icons">add</span>
+                    </button>
+                </div>
+            </div>
+            <div className="cards-container">
+                <div className="card">
+                    <img src = "brave_xCZFNihyOb-removebg-preview.png"></img>
+                    <div className="title-container">
+                        <span>{"heap implementation"}</span>
+                        <span>Language: {"Java"}</span>
+                    </div>
+                    <div className="card-item">
+                        <span className="material-icons">edit</span>
+                        <span className="material-icons">delete</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src = "brave_xCZFNihyOb-removebg-preview.png"></img>
+                    <div className="title-container">
+                        <span>{"heap implementation"}</span>
+                        <span>Language: {"Java"}</span>
+                    </div>
+                    <div className="card-item">
+                        <span className="material-icons">edit</span>
+                        <span className="material-icons">delete</span>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src = "brave_xCZFNihyOb-removebg-preview.png"></img>
+                    <div className="title-container">
+                        <span>{"heap implementation"}</span>
+                        <span>Language: {"Java"}</span>
+                    </div>
+                    <div className="card-item">
+                        <span className="material-icons">edit</span>
+                        <span className="material-icons">delete</span>
+                    </div>
+                </div>
                 <div className="card">
                     <img src = "brave_xCZFNihyOb-removebg-preview.png"></img>
                     <div className="title-container">
